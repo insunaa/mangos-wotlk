7a43e0e0efcdad6c4a35b05e49d4d4f8387c7027
insunaa <dev.insuna@gmail.com>
2022-07-23 01:36:19 +0200

    make gluth zombies mindless on decimate

--- src/game/AI/ScriptDevAI/scripts/northrend/naxxramas/boss_gluth.cpp
+++ src/game/AI/ScriptDevAI/scripts/northrend/naxxramas/boss_gluth.cpp
@@ -138,7 +138,11 @@ struct boss_gluthAI : public ScriptedAI
         for (GuidList::const_iterator itr = m_lZombieChowGuidList.begin(); itr != m_lZombieChowGuidList.end(); ++itr)
         {
             if (Creature* pZombie = m_creature->GetMap()->GetCreature(*itr))
+            {
+                pZombie->AttackStop(true, true);
+                pZombie->AI()->SetReactState(REACT_PASSIVE);
                 pZombie->GetMotionMaster()->MoveChase(m_creature, ATTACK_DISTANCE);
+            }
         }
     }
 
