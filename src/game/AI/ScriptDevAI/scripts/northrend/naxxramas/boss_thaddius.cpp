abfaad55e617ee1b588bbf9b854c56c312a45b5e
insunaa <dev.insuna@gmail.com>
2022-08-14 12:06:58 +0200

    Thaddius-Add: nullptr guard

--- src/game/AI/ScriptDevAI/scripts/northrend/naxxramas/boss_thaddius.cpp
+++ src/game/AI/ScriptDevAI/scripts/northrend/naxxramas/boss_thaddius.cpp
@@ -534,6 +534,8 @@ struct boss_thaddiusAddsAI : public BossAI
 
         JustDied(attacker);                                  // Texts
         AddCustomAction(THADDIUS_ADD_REVIVE, 5s, [&](){
+            if(!GetOtherAdd())
+                return;
             if (auto otherAI = dynamic_cast<boss_thaddiusAddsAI*>(GetOtherAdd()->AI()))
             {
                 if (otherAI->IsCountingDead())
