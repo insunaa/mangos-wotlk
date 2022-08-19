e0b473a4735b78565364fe00eff8a7394ff31831
insunaa <dev.insuna@gmail.com>
2022-08-19 22:43:50 +0200

    Thaddius: Debug movement

--- src/game/AI/ScriptDevAI/scripts/northrend/naxxramas/boss_thaddius.cpp
+++ src/game/AI/ScriptDevAI/scripts/northrend/naxxramas/boss_thaddius.cpp
@@ -338,7 +338,8 @@ struct boss_thaddiusAddsAI : public BossAI
     {
         SetDataType(TYPE_THADDIUS);
         AddCustomAction(THADDIUS_ADD_HOLD, true, [&](){
-            SetCombatMovement(true);
+            sLog.outError("HOLD CALLED!");
+            SetRootSelf(false);
             m_creature->GetMotionMaster()->MoveChase(m_creature->GetVictim());
         });
         AddCustomAction(THADDIUS_ADD_SHOCK_OVERLOAD, true, [&](){
@@ -454,7 +455,7 @@ struct boss_thaddiusAddsAI : public BossAI
 
     void PauseCombatMovement()
     {
-        SetCombatMovement(false);
+        SetRootSelf(true);
         ResetTimer(THADDIUS_ADD_HOLD, 1s + 500ms);
     }
 
